b85d498df2c4540884c5f61de635c6479197ff85
Ry Ma <ncxkomo@gmail.com>
2013-06-11 10:49:45 +0200

    Created Vasebook server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vasebook::Application.config.secret_token = '5b9cb0845fe75048fbdaefab3d0dc98bc48585cf02cb126b5944e5dd30501613f0dbfb2dc2e13c0a54cdd50a4ccc1f09846217cfd38698fb5f47f6aebe060468'
