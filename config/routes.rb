6847ecf2df4de89c93cee9460b74064058a9e19e
Ry Ma <ncxkomo@gmail.com>
2013-06-12 17:16:19 +0200

    Fleshed out profiles controller and tests, and added profile route.

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Vasebook::Application.routes.draw do
   root to: 'statuses#index'
 
 
+  get '/:id', to: 'profiles#show'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
