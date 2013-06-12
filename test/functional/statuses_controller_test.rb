24696e6b690630faa4df8e88cdd29d7f878bd564
Ry Ma <ncxkomo@gmail.com>
2013-06-12 16:15:20 +0200

    Added user authentication and tests to statuses controller.

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -11,16 +11,30 @@ class StatusesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:statuses)
   end
 
-  test "should get new" do
+  test "should be logged in to get new status page" do
+    get :new
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should get to new status page when logged in" do
+    sign_in users(:rydawg)
     get :new
     assert_response :success
   end
 
-  test "should create status" do
-    assert_difference('Status.count') do
-      post :create, status: { content: @status.content, name: @status.name }
-    end
+  test "should be logged in to create a status" do
+    post :create, status: { content: "Yo." }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
 
+  test "should be able to create a status when logged in" do
+    sign_in users(:rydawg)
+    assert_difference('Status.count') do
+      post :create, status: { content: "Yo." }
+    end
+    assert_response :redirect
     assert_redirected_to status_path(assigns(:status))
   end
 
@@ -29,21 +43,41 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should be logged in to get to edit status page" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should be able get to edit status page when logged in" do
+    sign_in users(:rydawg)
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
-    put :update, id: @status, status: { content: @status.content, name: @status.name }
+  test "should be logged in to update status" do
+    put :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should be able to update status when logged in" do
+    sign_in users(:rydawg)
+    put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
 
-  test "should destroy status" do
+  test "should be logged in to destroy status" do
+    delete :destroy, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+  
+  test "should be able to destroy status when logged in" do
+    sign_in users(:rydawg)
     assert_difference('Status.count', -1) do
       delete :destroy, id: @status
     end
-
     assert_redirected_to statuses_path
   end
 end
