4deeead41d523fc91a7641d7053496a5c2426416
Ry Ma <ncxkomo@gmail.com>
2013-06-12 16:32:13 +0200

    Added status validation and testing.

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,13 @@
 class Status < ActiveRecord::Base
   attr_accessible :content, :user_id
-
   belongs_to :user
+
+  validates :content, presence: :true,
+  					  length: { 
+  					  	minimum: 2,
+  					  	message: "Must contain more than one character."
+  					  }
+
+  validates :user_id, presence: :true
+  					  
 end
--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "that a status requires content" do 
+  	status = Status.new
+  	assert !status.save
+  	assert !status.errors[:content].empty?
+  end
+
+  test "that a status requires at least two characters" do 
+  	status = Status.new(content: "Y")
+  	assert !status.save
+  	assert status.errors[:content].include?("Must contain more than one character.")
+  end
+
+  test "that a status has a user id" do 
+  	status = Status.new(content: "Yo.")
+  	assert !status.save
+  	assert !status.errors[:user_id].empty?
+  end
+  
+
 end
